394fba81618e997b8b7a0c5943f50576d2bf5f03
AidanZealley <aidan@zealley.com>
2024-06-03 22:25:23 +0100

    Add ble stuff and change can transceiver script

--- can_transceiver.py
+++ can_transceiver.py
@@ -3,53 +3,52 @@ import can
 can_interface = 'can0'
 bus = can.interface.Bus(can_interface, bustype='socketcan', bitrate=100000)
 can_ids = [
-    0x0F6
+    0x123
+]
+can_modules = [
+    0x12
+]
+keys = [
+    0x01
 ]
 
-def send_message(can_id, key, value):
-    try:
-        # Determine the number of bytes needed to represent the value
-        can_dlc = 1  # Start with 1 byte for the key
-        if value <= 0xFF:
-            can_dlc += 1
-        elif value <= 0xFFFF:
-            can_dlc += 2
-        elif value <= 0xFFFFFF:
-            can_dlc += 3
-        else:
-            can_dlc += 4
-        
-        # Construct the data bytes for the CAN message
-        data = bytearray()
-        data.append(ord(key))  # Set the key in the first byte of data
-        for i in range(can_dlc - 1):
-            data.append((value >> (8 * i)) & 0xFF)  # Set the value in the following bytes
-        
-        # Create a CAN message object
-        message = can.Message(arbitration_id=can_id, data=data)
-        
-        # Send the CAN message
-        bus.send(message)
-        
-        print(f"Message sent with CAN ID: {can_id}, Key: {key}, Value: {value}")
-        return True
+def send_can_message(can_id, target_module, key, value):
+    key_bytes = key.to_bytes(1, byteorder='big')
+    value_bytes = value.to_bytes(4, byteorder='big')
+    data = target_module.to_bytes(1, byteorder='big') + key_bytes + value_bytes
+    message = can.Message(arbitration_id=can_id, data=data, is_extended_id=False)
     
-    except can.CanError as e:
-        print(f"Error: Failed to send CAN message. {e}")
-        return False
+    bus.send(message)
+
+def read_can_message(message):
+    can_id = message.arbitration_id
+    data = message.data
+    target_module = int.from_bytes(data[0:1], byteorder='big')
+    key = int.from_bytes(data[1:2], byteorder='big')
+    value = int.from_bytes(data[2:6], byteorder='big')
+    
+    return can_id, target_module, key, value
 
 
 def receive_can_message():
     try:
         while True:
-            message = bus.recv()  # Wait until a message is received
+            message = bus.recv()
             if message.arbitration_id in can_ids:
-                key = chr(message.data[0])
-                value = 0
-                for i in range(1, message.dlc):
-                    value |= message.data[i] << (8 * (i - 1))
-                    print(f"Received - Key: {key}, Value: {value}")
-                    send_message(can_ids[0], "B", 67890)
+                parsed_message = read_can_message(message)
+
+                can_id = parsed_message.arbitration_id
+                target_module = parsed_message.data[0]
+                key = parsed_message.data[1]
+                value_bytes = parsed_message.data[2:]
+                value = int.from_bytes(value_bytes, byteorder='big')
+
+                print("CAN ID:", hex(can_id))
+                print("Target Module:", hex(target_module))
+                print("Key:", hex(key))
+                print("Value:", value)
+
+                send_can_message(can_ids[0], can_modules[0], keys[0], 123)
 
     except KeyboardInterrupt:
         print("Program interrupted")
