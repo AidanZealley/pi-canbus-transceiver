21290210dc00d7c65ba1aa44d88834c7b3513301
AidanZealley <aidan@zealley.com>
2024-06-06 23:17:48 +0100

    Update characteristic id

--- canbus/can_transceiver.py
+++ canbus/can_transceiver.py
@@ -2,18 +2,18 @@ import asyncio
 from canbus.handler import CANHandler
 
 class CANMessageSubscriber:
-    async def notify(self, count_value):
+    def notify(self, count_value):
         print("Received message - Value:", count_value)
 
-async def main():
+def main():
     CAN_ID = 0x123
     MODULE_ID = 0x12
 
     can_handler = CANHandler(can_id=CAN_ID, module_id=MODULE_ID)
     subscriber = CANMessageSubscriber()
-    
-    await can_handler.add_subscriber(subscriber)
-    await can_handler.receive_can_message()
+
+    can_handler.add_subscriber(subscriber)
+    can_handler.receive_can_message()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
